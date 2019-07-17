49adabdbadd82472aecb0c039c9ca31cd31fda2b
fyeung21 <yeung.fiona@hotmail.com>
2019-07-16 19:43:35 -0700

    initial styling of footer

--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -1,22 +1,31 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * @package RED_Starter_Theme
- */
-
-?>
-
-			</div><!-- #content -->
+			</div>
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+				<section class="site-info">
+					<div class="contact">
+						<h2>Contact</h2>
+						<p>info@inhabitent.com</p>
+						<p>778-456-7891</p>
 
-		<?php wp_footer(); ?>
+					</div>
+					<div class="business-hours">
+						<h2>Business Hours</h2>
+						<p><span>Monday-Friday:</span> 9am to 5pm</p>
+						<p><span>Saturday:</span> 10am to 2pm</p>
+						<p><span>Sunday:</span> Closed</p>
+					</div>
+					<div class="footer-logo"></div>
+					
+					<div class="copyright">
+						<p>copyright © 2019 inhabitent</p>
+						<a href="<?php echo esc_url('https://wordpress.org/'); ?>"><?php printf(esc_html('Proudly powered by %s'), 'WordPress'); ?></a>
+					</div>
+				</section>
+			</footer>
+			</div>
 
-	</body>
-</html>
+			<?php wp_footer(); ?>
+
+			</body>
+
+			</html>
