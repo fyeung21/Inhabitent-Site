5a12678ba59b038a620966be193e827c9e885105
fyeung21 <yeung.fiona@hotmail.com>
2019-07-18 18:40:19 -0700

    started styling header

--- themes/inhabitent-theme/header.php
+++ themes/inhabitent-theme/header.php
@@ -25,7 +25,8 @@
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
-
+				
+				<!-- <div class="inhabitent-logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"></a></div> -->
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
