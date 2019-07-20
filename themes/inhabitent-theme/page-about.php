092292fe425c3a0d53bfd3d66a3ed94508463778
fyeung21 <yeung.fiona@hotmail.com>
2019-07-20 13:47:15 -0700

    started to style about page

--- themes/inhabitent-theme/page-about.php
+++ themes/inhabitent-theme/page-about.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+<div id="primary" class="content-area about-container">
+    <main id="main" class="site-main " role="main">
 
         <?php while ( have_posts() ) : the_post(); ?>
 
@@ -12,5 +12,4 @@
     </main><!-- #main -->
 </div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
