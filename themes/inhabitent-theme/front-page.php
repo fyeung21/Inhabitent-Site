acedc1daf025149d779182c9e6d9a07a0d1061c2
fyeung21 <yeung.fiona@hotmail.com>
2019-07-22 14:31:17 -0700

    front page has basic styling

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -2,42 +2,134 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+        <?php if (is_home() && !is_front_page()) : ?>
+            <header>
+                <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+            </header>
+        <?php endif; ?>
 
-            <?php  $args = array(
-                'order' => 'ASC',
-                'posts_per_page' => 4,
-                'post_type' => 'post',
-             ); ?>
-        <?php $journal_posts = new WP_Query( $args ); ?>   
+        <section class="home-hero-image">
+            <!-- hero image here -->
+            <div class="tent-logo">
+                <!-- logo -->
+            </div>
+        </section>
 
-        <?php if ($journal_posts->have_posts()) : ?>
+        <section class="home-container">
 
-             <?php while ($journal_posts->have_posts() ) : $journal_posts->the_post(); ?>
+            <h1>shop stuff</h1>
 
-                <h1><?php the_title(); ?></h1>
-                <?php the_post_thumbnail(); ?>
+            <article class="home-shop-grid">
 
-            <?php endwhile; ?>
+                <div class="home-shop-item">
+                    <div class="shop-icon">
+                        <img src="images/product-type-icons/do.svg">
+                    </div>
+                    <div class="category-description">
+                        <p>Get back to nature with all the tools and toys you need to enjoy the great outdoors.</p>
+                    </div>
+                    <div class="category-btn">
+                        <p><a href="<?php the_permalink(); ?>">do stuff</a></p>
+                    </div>
+                </div>
 
-            <?php the_posts_navigation(); ?>
+                <div class="home-shop-item">
+                    <div class="shop-icon">
+                        <img src="images/product-type-icons/eat.svg">
+                    </div>
+                    <div class="category-description">
+                        <p>Nothing beats food cooked over a fire. We have all you need for good camping eats.</p>
+                    </div>
+                    <div class="category-btn">
+                        <p><a href="<?php the_permalink(); ?>">eat stuff</a></p>
+                    </div>
+                </div>
 
-            <?php else: ?>
+                <div class="home-shop-item">
+                    <div class="shop-icon">
+                        <img src="images/product-type-icons/sleep.svg">
+                    </div>
+                    <div class="category-description">
+                        <p>Get a good night's rest in the wild in a home away from home that travels well.</p>
+                    </div>
+                    <div class="category-btn">
+                        <p><a href="<?php the_permalink(); ?>">sleep stuff</a></p>
+                    </div>
+                </div>
 
-                <h2>Cannot Find Recent Posts</h2>
+                <div class="home-shop-item">
+                    <div class="shop-icon">
+                        <img src="images/product-type-icons/wear.svg">
+                    </div>
+                    <div class="category-description">
+                        <p>From flannel shirts to toques, look the part while roughing it in the great outdoors.</p>
+                    </div>
+                    <div class="category-btn">
+                        <p><a href="<?php the_permalink(); ?>">wear stuff</a></p>
+                    </div>
+                </div>
 
-            <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
+            </article>
+
+        </section>
 
 
-		</main>
-	</div>
+        <section class="home-container">
+
+            <h1>inhabitent journal</h1>
+
+            <article class="home-post-grid">
+
+                <?php $args = array(
+                    'order' => 'ASC',
+                    'posts_per_page' => 3,
+                    'post_type' => 'post',
+                ); ?>
+                <?php $journal_posts = new WP_Query($args); ?>
+
+                <?php if ($journal_posts->have_posts()) : ?>
+
+                    <?php while ($journal_posts->have_posts()) : $journal_posts->the_post(); ?>
+
+                        <div class="home-post-item">
+
+                            <div class="post-image">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+
+                            <div class="post-info-container">
+
+                                <div class="entry-meta">
+                                    <p><?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?></p>
+                                </div>
+
+                                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+                                <p><a class="read-more" href="<?php the_permalink(); ?>">read entry →</a></p>
+                            </div>
+
+                        </div>
+
+                    <?php endwhile; ?>
+
+                    <?php the_posts_navigation(); ?>
+
+                <?php else : ?>
+
+                    <h2>Cannot Find Recent Posts</h2>
+
+                <?php endif; ?>
+                <?php wp_reset_postdata(); ?>
+
+            </article>
+
+        </section>
+
+
+    </main>
+</div>
 
 <?php get_footer(); ?>
