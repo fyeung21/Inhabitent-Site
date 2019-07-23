c0a774ed3a702be4fc546b8c952b33b52c23dfbc
fyeung21 <yeung.fiona@hotmail.com>
2019-07-23 14:56:37 -0700

    finalized styling for footer

--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -1,28 +1,31 @@
 			</div>
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<section class="site-info">
-					<div class="contact">
-						<h2>Contact</h2>
-						<p><span><i class="fa fa-envelope" aria-hidden="true"></i></span>info@inhabitent.com</p>
-						<p><span><i class="fa fa-phone" aria-hidden="true"></i></span>778-456-7891</p>
-						<span><i class="fa fa-facebook-square" aria-hidden="true"></i></span>
-						<span><i class="fa fa-twitter-square" aria-hidden="true"></i></span>
-						<span><i class="fa fa-google-plus-square" aria-hidden="true"></i></span>
+				<div class="footer-background" style="background-image:url(<?php bloginfo('template_directory'); ?>/images/dark-wood.png">
+					<section class="site-info">
+						<div class="contact">
+							<h2>Contact</h2>
+							<p><span><i class="fa fa-envelope" aria-hidden="true"></i></span>info@inhabitent.com</p>
+							<p><span><i class="fa fa-phone" aria-hidden="true"></i></span>778-456-7891</p>
+							<span><i class="fa fa-facebook-square" aria-hidden="true"></i></span>
+							<span><i class="fa fa-twitter-square" aria-hidden="true"></i></span>
+							<span><i class="fa fa-google-plus-square" aria-hidden="true"></i></span>
 
-					</div>
-					<div class="business-hours">
-						<h2>Business Hours</h2>
-						<p><span>Monday-Friday:</span> 9am to 5pm</p>
-						<p><span>Saturday:</span> 10am to 2pm</p>
-						<p><span>Sunday:</span> Closed</p>
-					</div>
-					<div class="footer-logo"></div>
-					
-				</section>
+						</div>
+						<div class="business-hours">
+							<h2>Business Hours</h2>
+							<p><span>Monday-Friday:</span> 9am to 5pm</p>
+							<p><span>Saturday:</span> 10am to 2pm</p>
+							<p><span>Sunday:</span> Closed</p>
+						</div>
+
+						<img class="footer-logo" src="<?php bloginfo('template_directory'); ?>/images/logos/inhabitent-logo-text.svg" alt="">
+
+					</section>
 					<div class="copyright">
 						<p>copyright © 2019 inhabitent</p>
 					</div>
+				</div>
 			</footer>
 			</div>
 
