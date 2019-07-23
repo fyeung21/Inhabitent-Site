87aa6431417aecfdc6df92170aee880c73a12b5b
fyeung21 <yeung.fiona@hotmail.com>
2019-07-23 11:20:08 -0700

    fixed headings and margins on basic template

--- themes/inhabitent-theme/template-parts/content-page.php
+++ themes/inhabitent-theme/template-parts/content-page.php
@@ -1,16 +1,8 @@
-<?php
-/**
- * Template part for displaying page content in page.php.
- *
- * @package RED_Starter_Theme
- */
-
-?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	</header>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
@@ -20,5 +12,5 @@
 				'after'  => '</div>',
 			) );
 		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+	</div>
+</article>
