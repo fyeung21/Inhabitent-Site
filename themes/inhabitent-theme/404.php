569afccdef8cf89daab75b7da3983036a4763254
fyeung21 <yeung.fiona@hotmail.com>
2019-08-13 20:50:22 -0700

    style 404 page to match design comp

--- themes/inhabitent-theme/404.php
+++ themes/inhabitent-theme/404.php
@@ -1,14 +1,4 @@
-<?php
-
-/**
- * The template for displaying 404 pages (not found).
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package RED_Starter_Theme
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
@@ -19,7 +9,7 @@ get_header(); ?>
 
 				<header class="page-header">
 					<h1 class="page-title"><?php echo esc_html('Oops! That page can&rsquo;t be found.'); ?></h1>
-				</header><!-- .page-header -->
+				</header>
 
 				<div class="page-content">
 					<p><?php echo esc_html('It looks like nothing was found at this location. Maybe try one of the links below or a search?'); ?></p>
@@ -58,7 +48,11 @@ get_header(); ?>
 				
 			</div>
 </div><!-- .page-content -->
+
+
 </section><!-- .error-404 -->
+
+<?php get_sidebar(); ?>
 
 </main><!-- #main -->
 </div><!-- #primary -->
